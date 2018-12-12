86df7fd8609d6a7e13ea2cbbc360b4764f937f44
TurkyKSA <44849716+TurkyKSA@users.noreply.github.com>
2018-12-12 05:32:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -236,19 +236,7 @@ client.on('message', function(message) {
       });
 
 
-      var antispam = require("anti-spam");//npm i anti-spam
- 
-      antispam(client, {
-        warnBuffer: 3, //الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على تحذير.
-        maxBuffer: 5, // الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على ميوت.
-        interval: 1000, // مقدار الوقت قبل حصول باند
-        warningMessage: "stop spamming.", // رسالة تحذير اذا سوا سبام!
-        roleMessage: "Muted!!", // الرسالة الي تجي اذا شخص اخذ ميوت
-        roleName: "Muted", // اسم رتبة الميوت
-        maxDuplicatesWarning: 7, // عدد الرسايل الي قبل التحذيرات
-        maxDuplicatesBan: 10, // عدد الرسايل الي يقدر المستخدم يرسلها قبل الميوت
-        time: 10, // عدد الوقت الي يجلس لين تسحب رتبة الميوت من الشخص الحسبة برمجية وليست كتابية 
-      });
+     
       
 
       client.on('message', message => {
@@ -1814,14 +1802,6 @@ const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));//require data.j
   })
 //By Alpha Codes And Edited By OrochiX
 //
-
-
-
-
-
-
-
-
 
 
 
